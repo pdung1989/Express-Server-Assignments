21888851baf6d03114f11f651bd9d94cb279bdb5
Dung Tran <pdung1989@gmail.com>
2021-10-29 15:31:58 +0300

    get route /cat/:Id

--- week2/app.js
+++ week2/app.js
@@ -4,7 +4,13 @@ const app = express();
 const port = 3000;
 
 app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.')
+  res.send('From this endpoint you can get cats.');
 });
 
+app.get('/cat/:catId', (req, res) => {
+  console.log('/cat route', req.params);
+  res.send(
+    `From the end point you can get specific cat from ${req.params.catId}`
+  );
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
