212a921ba96834f5b7c791228bd876c415acd6d0
Dung Tran <pdung1989@gmail.com>
2021-11-02 12:07:48 +0200

    add middleware upload()

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,6 +1,12 @@
 'use strict';
 // catRoute
 const express = require('express');
+
+// multer module to handle multipart/form-data because express does not handle it
+const multer = require('multer');
+// create upload middleware
+const upload = multer({dest: './uploads/'});
+
 const { cat_list_get, cat_get, cat_post } = require('../controllers/catController');
 const router = express.Router(); //use to create routes
 
@@ -8,7 +14,7 @@ router.get('/', cat_list_get);
 
 router.get('/:catId', cat_get);
 
-router.post('/', cat_post);
+router.post('/',  upload.single('cat'), cat_post);
 
 router.put('/', (req, res) => {
   res.send('From this endpoint you can update cat.');
