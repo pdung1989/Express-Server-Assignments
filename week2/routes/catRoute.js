8842e457399f4983de4f3becd7a66202aa5cbdfb
Dung Tran <pdung1989@gmail.com>
2021-11-02 13:37:49 +0200

    create database branch, install dotenv, mysql2

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -5,16 +5,21 @@ const express = require('express');
 // multer module to handle multipart/form-data because express does not handle it
 const multer = require('multer');
 // create upload middleware
-const upload = multer({dest: './uploads/'});
+const upload = multer({ dest: './uploads/' });
 
-const { cat_list_get, cat_get, cat_post } = require('../controllers/catController');
+const {
+  cat_list_get,
+  cat_get,
+  cat_post,
+} = require('../controllers/catController');
 const router = express.Router(); //use to create routes
 
 router.get('/', cat_list_get);
 
 router.get('/:catId', cat_get);
 
-router.post('/',  upload.single('cat'), cat_post);
+// add upload middleware
+router.post('/', upload.single('cat'), cat_post);
 
 router.put('/', (req, res) => {
   res.send('From this endpoint you can update cat.');
