25ff7776f0b0048097d9b205a02fdeef634bd7f3
Dung Tran <pdung1989@gmail.com>
2021-11-02 14:16:54 +0200

    get all cats

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -2,16 +2,18 @@
 /* catController*/
 
 // object detructuring, import only cats from catModel
-const { cats, getCat } = require('../models/catModel');
+const { getAllCats, getCat } = require('../models/catModel');
 
-const cat_list_get = (req, res) => {
+const cat_list_get = async (req, res) => {
+  const cats = await getAllCats();
+  console.log('all cats', cats);
   res.json(cats); //can use: res.send(cats)
 };
 
 const cat_get = (req, res) => {
   const cat = getCat(req.params.catId);
   res.json(cat);
-}
+};
 
 const cat_post = (req, res) => {
   console.log('add cat data', req.body);
--- week2/models/catModel.js
+++ week2/models/catModel.js
@@ -1,29 +1,26 @@
 'use strict';
-const cats = [
-  {
-    id: '1',
-    name: 'Frank',
-    birthdate: '2010-10-30',
-    weight: '5',
-    owner: '1',
-    filename: 'http://placekitten.com/400/300',
-  },
-  {
-    id: '2',
-    name: 'James',
-    birthdate: '2015-12-25',
-    weight: '11',
-    owner: '2',
-    filename: 'http://placekitten.com/400/302',
-  },
-];
+
+const pool = require('../database/db');
+const promisePool = pool.promise();
 
 const getCat = (catId) => {
   // TODO find single cat from cats-array and return it
   return cats.find(cat => cat.id == catId);
 };
 
+// use async await to handle fetching data
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
+
 module.exports = {
-  cats,
   getCat,
+  getAllCats,
 };
